90dea92e8c20e9ca321e2f7d57ad37a3fb2d4203
Luca Severini <lucaseverini@mac.com>
2015-11-08 11:58:23 -0800

    Update Reachability.m
    The changes fix the leak reported by the Analyzer in XCode 7. Issue #114

--- Reachability.m
+++ Reachability.m
@@ -98,8 +98,9 @@ static void TMReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
     SCNetworkReachabilityRef ref = SCNetworkReachabilityCreateWithName(NULL, [hostname UTF8String]);
     if (ref) 
     {
-        id reachability = [[self alloc] initWithReachabilityRef:ref];
-
+        id reachability = [[self alloc] initWithReachabilityRef:CFBridgingRetain((__bridge id)ref)];
+	CFRelease(ref);
+	
         return reachability;
     }
     
@@ -111,7 +112,8 @@ static void TMReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
     SCNetworkReachabilityRef ref = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, (const struct sockaddr*)hostAddress);
     if (ref) 
     {
-        id reachability = [[self alloc] initWithReachabilityRef:ref];
+        id reachability = [[self alloc] initWithReachabilityRef:CFBridgingRetain((__bridge id)ref)];
+        CFRelease(ref);
         
         return reachability;
     }
