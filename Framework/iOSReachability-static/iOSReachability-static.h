67503271d5654a3715912092c57bf34cb5f7262b
Alexander Dodatko <alexander.dodatko@dataart.com>
2016-07-22 11:48:12 +0300

    added static lib and static framework target

---
+++ Framework/iOSReachability-static/iOSReachability-static.h
@@ -0,0 +1,19 @@
+//
+//  iOSReachability-static.h
+//  iOSReachability-static
+//
+//  Created by Alexander Dodatko on 7/22/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSReachability-static.
+FOUNDATION_EXPORT double iOSReachability_staticVersionNumber;
+
+//! Project version string for iOSReachability-static.
+FOUNDATION_EXPORT const unsigned char iOSReachability_staticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSReachability_static/PublicHeader.h>
+
+
